c27202f884edcad4a7d038f21f7925a7b8d25288
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-03-01 13:57:32 -0600

    Update sistema_info.sh

--- sistema_info.sh
+++ sistema_info.sh
@@ -151,7 +151,12 @@ opensshport="$(netstat -ntlp | grep -i ssh | grep -i 0.0.0.0 | awk '{print $4}' 
 dropbearport="$(netstat -nlpt | grep -i dropbear | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 stunnel4port="$(netstat -nlpt | grep -i stunnel | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 openvpnport="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+if [ -f /etc/squid3/squid.conf ];
+then
+squidport="$(cat /etc/squid3/squid.conf | grep -i http_port | awk '{print $2}')"
+else
 squidport="$(cat /etc/squid/squid.conf | grep -i http_port | awk '{print $2}')"
+fi
 PUERTOUNIVERSAL="$(cat /etc/default/sslh | grep -i listen | awk '{print $4}')"
 echo -e "<br><font color=\"#FFBF38\">=========: PUERTOS ACTIVOS :=========</font><br>"
 echo -e "<br><font color=\"#77C500\">         Port 443        :  </font>"$(cat /etc/default/sslh | grep -i listen | awk '{print $4}')
@@ -160,4 +165,4 @@ echo -e "<br><font color=\"#77C500\">         Port Dropbear   :  </font>"$dropbe
 echo -e "<br><font color=\"#77C500\">         Port SSL        :  </font>"$stunnel4port
 echo -e "<br><font color=\"#77C500\">         Port Squid      :  </font>"$squidport
 echo -e "<br><font color=\"#77C500\">         Port OpenVPN    :  </font>"$openvpnport
-
+rm $0
