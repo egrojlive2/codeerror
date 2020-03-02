b29a67a9a8fe321f7b9ebdb5fab60b8e7c08ed09
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-03-02 10:21:31 -0600

    Update sistema_info.sh

--- sistema_info.sh
+++ sistema_info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+apt install net-tools -y
 ########################################################################
 # Color esthetics
 ########################################################################
@@ -157,9 +157,14 @@ squidport="$(cat /etc/squid3/squid.conf | grep -i http_port | awk '{print $2}')"
 else
 squidport="$(cat /etc/squid/squid.conf | grep -i http_port | awk '{print $2}')"
 fi
-PUERTOUNIVERSAL="$(cat /etc/default/sslh | grep -i listen | awk '{print $4}')"
+if [ -f /etc/default/sslh ];
+then
+puertouniversal="$(cat /etc/default/sslh | grep -i listen | awk '{print $4}')"
+else
+puertouniversal="no configurado"
+fi
 echo -e "<br><font color=\"#FFBF38\">=========: PUERTOS ACTIVOS :=========</font><br>"
-echo -e "<br><font color=\"#77C500\">         Port 443        :  </font>"$(cat /etc/default/sslh | grep -i listen | awk '{print $4}')
+echo -e "<br><font color=\"#77C500\">         Port 443        :  </font>"$puertouniversal
 echo -e "<br><font color=\"#77C500\">         Port OpenSSH    :  </font>"$opensshport
 echo -e "<br><font color=\"#77C500\">         Port Dropbear   :  </font>"$dropbearport
 echo -e "<br><font color=\"#77C500\">         Port SSL        :  </font>"$stunnel4port
