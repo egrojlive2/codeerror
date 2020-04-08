d55dc3d53fd33cd66b4cb1084271373f086504ea
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-04-07 22:13:39 -0500

    Update u_ssh.sh

--- u_ssh.sh
+++ u_ssh.sh
@@ -4,18 +4,20 @@ rm ONLINE > /dev/null 2>&1;rm ssh.txt > /dev/null 2>&1;rm login-db.txt > /dev/nu
 data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
 NUM2=`ps aux | grep sshd | grep -v root | grep -v debian | grep -v admin | wc -l`;
 
-echo "USUARIOS OPENSSH $NUM2"
+echo '<font color=\"#FFBF38\">Usuarios Openssh '$((NUM2))'</font><br>'
 #echo "$data"
 for PID in "${data[@]}"
 do
         #echo "check $PID";
         NUM2=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | wc -l`;
         USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
-        total=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}' | grep $USER | wc -l` 
+        total=$(cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}' | grep $USER | wc -l) 
         IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
         if [ $NUM2 -eq 1 ]; then
                 echo "$USER" >> ssh.txt;
                 #echo "[TOTAL en OpenSSH]: $NUM2";
         fi
 done
+if [ -f ssh.txt ]; then
 sed 's/\\s/\\n/g' ssh.txt | sort | uniq  -c | sort -n | sed 's/$/<br>/'
+fi
