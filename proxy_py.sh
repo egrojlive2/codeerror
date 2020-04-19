a320d4a4261a20334441d464f8678bad24ae9934
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-04-19 01:41:10 -0500

    Create proxy_py.sh

---
+++ proxy_py.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+echo "INSTALANDO PROXY PYTHON"
+echo
+echo
+if [ -f /etc/code/proxy.py ]; then
+chmod +x /etc/code/proxy.py > /dev/null 2>&1;
+else
+mkdir /etc/code > /dev/null 2>&1;
+wget https://github.com/egrojlive/codeerror/raw/master/pr_py.py -O /etc/code/proxy.py > /dev/null 2>&1;
+chmod +x /etc/code/proxy.py > /dev/null 2>&1;
+fi
+sed -i "s/8080/8081/g" /etc/default/sslh > /dev/null 2>&1;
+service sslh restart > /dev/null 2>&1;
+
+echo "[Unit]
+Description=SERVICIO proxy python en escucha puerto 8081
+
+[Service]
+Type=ilde
+ExecStart=python /etc/code/proxy.py
+User=root
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/proxypy.service;
+
+chmod +x /etc/systemd/system/proxypy.service > /dev/null 2>&1;
+systemctl daemon-reload > /dev/null 2>&1;
+systemctl restart proxypy.service > /dev/null 2>&1;
+echo "PROXY INSTALADO EN PUERTO 8081"
