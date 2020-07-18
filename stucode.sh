3718d4c70f90431c8d5fe0aa3f52accd8493a196
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-07-18 17:46:52 -0500

    Update stucode.sh

--- stucode.sh
+++ stucode.sh
@@ -15,21 +15,28 @@ mkdir /etc/code > /dev/null 2>&1;
 echo "[Unit]
 Description=SERVICIO Stunnel Code Redirect Puerto 442
 After=network.target
+After=syslog.target
 
 [Service]
-Type=ilde
+Type=forking
 ExecStart=/usr/bin/stunnel4 /etc/code/stunnelcode.conf
+ExecStop=/usr/bin/killall -9 stunnel
+TimeoutSec=600
+
+Restart=always
 User=root
 Restart=on-failure
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/stunnelcode.service;
 
-
 chmod +x /etc/systemd/system/stunnelcode.service > /dev/null 2>&1;
 systemctl daemon-reload > /dev/null 2>&1;
 systemctl enable stunnelcode.service > /dev/null 2>&1;
 systemctl restart stunnelcode.service > /dev/null 2>&1;
+echo "Redireccionado Puerto 442 Para Ssl tls tunnel"
+echo
 else
 echo "Primero Debes Instalar El Servicio Stunnel4"
+echo
 fi
 rm $0 > /dev/null 2>&1;
