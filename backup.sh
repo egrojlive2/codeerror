513ecd7b3b308ed4acea16a799f38103cfbfc7c5
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-10-11 13:46:36 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,6 +6,6 @@ us1=$(echo $usuario | cut -d ":" -f1)
 fecha=$(echo $usuario | cut -d ":" -f2)
 pass1=$(echo $usuario | cut -d ":" -f3)
 limite=$(echo $usuario | cut -d ":" -f4)
-datos=$(printf "%s:%s" "$us1" "$fecha" "$pass1" "$datos")
+datos=$(printf "%s:%s:%s:$s\n" "$us1" "$fecha" "$pass1" "$datos")
 echo $datos
 done
