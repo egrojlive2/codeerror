c56865318f463da9b8d17766828420f1ab6c8910
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-10-11 12:47:56 -0500

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+lista=$(grep /home/ /etc/passwd | grep -v syslog | grep -v root | cut -d ":" -f1,5 | grep -v ntp | grep -v debian)
+for usuario in $lista;
+do
+us1=$(echo $usuario | cut -d ":" -f1)
+fecha=$(echo $usuario | cut -d ":" -f2)
+pass1=$(echo $usuario | cut -d ":" -f3)
+limite=$(echo $usuario | cut -d ":" -f4)
+datos=$(printf "%s:%s" "$us1" "$fecha" "$pass1" "$datos")
+echo $datos
+done
