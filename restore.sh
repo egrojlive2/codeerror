fdb868146c8264c9b9959a52b60c5b0e31322815
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-10-11 18:28:38 -0500

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -14,14 +14,14 @@ echo "$us1:$pass1" | chpasswd
 chfn -r $pass1 $us1 > /dev/null 2>&1;
 fi
 if [ $fecha ]; then
-chage -E $fecha $us1
-chfn -f $fecha $us1 > /dev/null 2>&1
+chage -E $fecha $us1;
+chfn -f $fecha $us1 > /dev/null 2>&1;
 else
 fecha=''
 fi
 if [ $limite ]; then
-chfn -w $limite $us1 > /dev/null 2>&1
-limitar $us1 $limite
+chfn -w $limite $us1 > /dev/null 2>&1;
+limitar $us1 $limite;
 fi
 done
 
