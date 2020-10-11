0755a2b18ce11d9c041f19492f99c43e3f135d71
egrojlive <52388526+egrojlive@users.noreply.github.com>
2020-10-11 16:19:24 -0500

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#chfn -f 2020/08/12 -h casa -r 3131003801 -w oficina -o mas jorge
-#jorge:2020/08/12,3131003801,oficina,casa,mas
 lista=$1
 if [ -d /etc/code/limite ]; then
 for usuario in $lista;
@@ -12,19 +10,18 @@ limite=$(echo $usuario | cut -d ":" -f4)
 pkill -u $us1 > /dev/null 2>&1;
 useradd $us1 > /dev/null 2>&1;
 if [ $pass1 ]; then
-echo "$us1:$pass1" | chpasswd;
+echo "$us1:pass1" | chpasswd
 fi
 if [ $fecha ]; then
-chage -E $fecha $us1;
-chfn -f $fecha $us1
+chage -E $fecha $us1
+chfn -f $fecha $us1 > /dev/null 2>&1
 else
 fecha=''
 fi
+echo $us1
 if [ $limite ]; then
-chfn -w $limite $us1
+chfn -w $limite $us1 > /dev/null 2>&1
 limitar $us1 $limite
-else
-limite=0
 fi
 done
 
