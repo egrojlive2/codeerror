1908345f4dc54afd1f10d90bcd00648ef146e3df
egrojlive <52388526+egrojlive@users.noreply.github.com>
2021-01-10 01:06:25 -0600

    Update stucode.sh

--- stucode.sh
+++ stucode.sh
@@ -3,6 +3,11 @@ if [[ "$USER" != 'root' ]]; then
   echo "Este Script Solo Funciona Para Usuarios root"
   exit
 fi
+
+if [ $(which stunnel) ]; then
+echo 'Por Favor Instala Stunnel Primero'
+exit 0
+fi
 ip=$1;
 puerto=$2;
 if [ $ip ]; then
@@ -16,41 +21,37 @@ escuchar=$puerto;
 else
 escuchar=442;
 fi
-if [ -f /etc/stunnel/stunnel.conf ]; then
 mkdir /etc/code > /dev/null 2>&1;
-echo "[STUNNELCOD]
+echo "[stunnelcode5]
 client = no
 cert = /etc/stunnel/stunnel.pem
-accept = $puerto
-connect = $1" > /etc/code/stunnelcode.conf;
+accept = $escuchar
+connect = $ip
+" > /etc/code/stunnelcode.conf
 echo "[Unit]
-Description=SERVICIO Stunnel Code Redirect Puerto $puerto
+Description=SSL StunnelCode 5
 After=network.target
 After=syslog.target
 
+[Install]
+WantedBy=multi-user.target
+Alias=stunnel.target
+
 [Service]
 Type=forking
-ExecStart=/usr/bin/stunnel4 /etc/code/stunnelcode.conf
-ExecStop=/usr/bin/killall -9 stunnel
+ExecStart=/usr/bin/stunnel /etc/code/stunnelcode.conf
+ExecStop=/usr/bin/pkill stunnel
+
 TimeoutSec=600
 
 Restart=always
-#User=root
-Restart=on-failure
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/stunnelcode.service;
+PrivateTmp=false" > /etc/systemd/system/stunnel5.service;
 
-chmod +x /etc/systemd/system/stunnelcode.service > /dev/null 2>&1;
+chmod +x /etc/systemd/system/stunnel5.service > /dev/null 2>&1;
 systemctl daemon-reload > /dev/null 2>&1;
-systemctl enable stunnelcode.service > /dev/null 2>&1;
-systemctl restart stunnelcode.service > /dev/null 2>&1;
-service stunnel4 restart > /dev/null 2>&1;
-service stunnel4 restart > /dev/null 2>&1;
+systemctl enable stunnel5.service > /dev/null 2>&1;
+systemctl restart stunnel5.service > /dev/null 2>&1;
 echo "Redireccionado Puerto $puerto Para Ssl tls tunnel"
 echo "Ahora ya solo coloca tu ip y el puerto $puerto"
 echo
-else
-echo "Primero Debes Instalar El Servicio Stunnel4"
-echo
-fi
 rm $0 > /dev/null 2>&1;
