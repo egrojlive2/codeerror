8112de4ac7bdd787ecb994812f6f29b828abf570
egrojlive <52388526+egrojlive@users.noreply.github.com>
2021-06-27 20:05:50 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ "$USER" != 'root' ]]; then
   echo "Este Script Solo Funciona Para Usuarios root"
+  RM $0 > /dev/null 2>&1
   exit
 fi
 lista=$(grep /home/ /etc/passwd | grep -v syslog | grep -v root | cut -d ":" -f1,5 | grep -v ntp | grep -v debian| sed "s/,/:/g"| sed "s/ /$/g")
@@ -12,4 +13,4 @@ pass1=$(echo $usuario | cut -d ":" -f3)
 limite=$(echo $usuario | cut -d ":" -f6)
 echo "$us1:$fecha:$pass1::$limite"
 done
-rm $0
+rm $0 > /dev/null 2>&1
