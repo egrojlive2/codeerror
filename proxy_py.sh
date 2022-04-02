950bf378b2e4a470076cf0378646feb786b673bf
egrojlive <52388526+egrojlive@users.noreply.github.com>
2022-04-02 15:15:39 -0600

    Update proxy_py.sh

--- proxy_py.sh
+++ proxy_py.sh
@@ -17,7 +17,7 @@ fi
 
 echo "Instalando Proxy En Puertos EL $puerto_primero Y $puerto_segundo"
 mkdir /etc/code > /dev/null 2>&1;
-if [ -f /etc/code/servidores.json]; then
+if [ -f /etc/code/servidores.json ]; then
 echo
 else
 echo "{'default':'127.0.0.1'}" > /etc/code/servidores.json
